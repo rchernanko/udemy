12a05c920dcbc98c9c52fff223f0b404c29f4ac0
rchernanko <richard.chernanko@marks-and-spencer.com>
2015-03-17 22:23:51 +0000

    More practice with getters and setters. Also started to explore this. in more detail

---
+++ src/This_1_Drink.java
@@ -0,0 +1,39 @@
+public class This_1_Drink {
+
+    private String drinkName;
+    private double drinkAlcoholStrength;
+    private int drinkAlcoholUnits;
+    private double drinkPrice;
+
+    public void setDrinkName(String drinkName){
+        this.drinkName = drinkName;
+    }
+
+    public String getDrinkName(){
+        return drinkName;
+    }
+
+    public void setDrinkAlcoholStrength(double drinkAlcoholStrength){
+        this.drinkAlcoholStrength = drinkAlcoholStrength;
+    }
+
+    public double getDrinkAlcoholStrength(){
+        return drinkAlcoholStrength;
+    }
+
+    public void setDrinkAlcoholUnits(int drinkAlcoholUnits){
+        this.drinkAlcoholUnits = drinkAlcoholUnits;
+    }
+
+    public int getDrinkAlcoholUnits(){
+        return drinkAlcoholUnits;
+    }
+
+    public void setDrinkPrice(double drinkPrice){
+        this.drinkPrice = drinkPrice;
+    }
+
+    public double getDrinkPrice(){
+        return drinkPrice;
+    }
+}

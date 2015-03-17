1ad4e722fa8538b26ec3a490d85ce475797d8bee
rchernanko <richard.chernanko@marks-and-spencer.com>
2015-03-17 10:25:27 +0000

    Method parameters + some more practice with getter methods

---
+++ src/Classes_8_Cities.java
@@ -0,0 +1,32 @@
+public class Classes_8_Cities {
+
+    public static void main(String[] args) {
+
+        Classes_8_City london = new Classes_8_City();
+        Classes_8_City rome = new Classes_8_City();
+        Classes_8_City paris = new Classes_8_City();
+
+        london.cityName = "London";
+        london.cityPopulation = 6_000_000;
+
+        System.out.println("This city is called : " + london.getCityName());
+        System.out.println("The population of " + london.getCityName() + " is " + london.getCityPopulation());
+        System.out.println("The estimated number of commuters in " + london.getCityName() + " is : " + london.getEstimatedNumberOfDailyCommuters());
+
+        rome.cityName = "Rome";
+        rome.cityPopulation = 3_000_000;
+
+        System.out.println(rome.getCityName());
+        System.out.println(rome.getCityPopulation());
+        System.out.println(rome.getEstimatedNumberOfDailyCommuters());
+
+        paris.cityName = "Paris";
+        paris.cityPopulation = 5_000_000;
+
+        System.out.println(paris.getCityName());
+        System.out.println(paris.getCityPopulation());
+        System.out.println(paris.getEstimatedNumberOfDailyCommuters());
+
+    }
+
+}

4323dbdbf9fcbfa430b4cdd7a815a29f109bfa0d
rchernanko <richard.chernanko@marks-and-spencer.com>
2015-03-17 20:37:13 +0000

    setters and getters

---
+++ src/Setters_And_Getters_2_Animal.java
@@ -0,0 +1,30 @@
+public class Setters_And_Getters_2_Animal {
+
+    private String animalName;
+    private String favouriteFood;
+    private int runningSpeed;
+
+    public void setAnimalName(String name){
+        animalName = name;
+    }
+
+    public String getAnimalName(){
+        return animalName;
+    }
+
+    public void setFavouriteFood(String food){
+        favouriteFood = food;
+    }
+
+    public String getFavouriteFood(){
+        return favouriteFood;
+    }
+
+    public void setRunningSpeed(int speed){
+        runningSpeed = speed;
+    }
+
+    public int getRunningSpeed(){
+        return runningSpeed;
+    }
+}

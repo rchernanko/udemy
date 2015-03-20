f4da8cd26d593892dc6ff1c2281590e291231216
rchernanko <richard.chernanko@marks-and-spencer.com>
2015-03-20 18:58:30 +0000

    in marc example constructor class, i am making my instance variables private and adding getters and setters. need to check that this is correct set-up with someone at work

--- src/Constructors_Marc_Example.java
+++ src/Constructors_Marc_Example.java
@@ -1,15 +1,35 @@
 public class Constructors_Marc_Example {
 
-    public String animalName;
-    public int animalAge;
+    private String animalName;
+    private int animalAge;
 
-    public Constructors_Marc_Example(){
-        this("Dog", 3);
+    public void setAnimalName(String animalName){
+        this.animalName = animalName;
     }
+
+    public String getAnimalName(){
+        return animalName;
+    }
+
+    public void setAnimalAge(int animalAge){
+        this.animalAge = animalAge;
+    }
+
+    public int getAnimalAge(){
+        return animalAge;
+    }
+
+    public Constructors_Marc_Example() {
+    }
+
+    //is this correct though???? Is this the right way to set up my private instance variables, with getters, setters and constructors?
 
     public Constructors_Marc_Example(String animalName, int animalAge){
         this.animalName = animalName;
         this.animalAge = animalAge;
+
+        System.out.println("My name is " + this.animalName + " and I have been created through a constructor");
+        System.out.println("My age is " + this.animalAge + " and I have been created through a constructor");
     }
 }
 
@@ -18,10 +38,22 @@ class Hello{
     public static void main(String[] args) {
 
         Constructors_Marc_Example test1 = new Constructors_Marc_Example();
-        System.out.println(test1.animalName + test1.animalAge);
 
-        Constructors_Marc_Example test2 = new Constructors_Marc_Example("Cat", 23);
-        System.out.println(test2.animalName + test2.animalAge);
+        test1.setAnimalName("Bailey");
+        test1.setAnimalAge(3);
+
+        System.out.println(test1.getAnimalName());
+        System.out.println(test1.getAnimalAge());
+
+        Constructors_Marc_Example test2 = new Constructors_Marc_Example();
+
+        test2.setAnimalName("Binky");
+        test2.setAnimalAge(6);
+
+        System.out.println(test2.getAnimalName());
+        System.out.println(test2.getAnimalAge());
+
+        Constructors_Marc_Example test3 = new Constructors_Marc_Example("Benny", 4);
 
     }
 

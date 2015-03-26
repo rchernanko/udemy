30c6b4ea77caa970efd575c48769e015265dcee5
rchernanko <richard.chernanko@marks-and-spencer.com>
2015-03-26 22:12:29 +0000

    adding an example of the toString() method

---
+++ src/ToStringMethodExample.java
@@ -0,0 +1,24 @@
+public class ToStringMethodExample {
+
+        int rollno;
+        String name;
+        String city;
+
+        ToStringMethodExample(int rollno, String name, String city){
+            this.rollno=rollno;
+            this.name=name;
+            this.city=city;
+        }
+
+    public String toString(){//overriding the toString() method
+        return rollno+" "+name+" "+city;
+    }
+
+    public static void main(String args[]){
+            ToStringMethodExample s1=new ToStringMethodExample(101,"Raj","lucknow");
+            ToStringMethodExample s2=new ToStringMethodExample(102,"Vijay","ghaziabad");
+
+            System.out.println(s1);//if i don't have the overriding toString() method, here the compiler writes here s1.toString()
+            System.out.println(s2);//if i don't have the overriding toString() method, here the compiler writes here s2.toString()
+        }
+    }

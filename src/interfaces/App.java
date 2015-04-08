d75d1b5304b6ee5a478d9e167faf7124b81a3307
rchernanko <rchernanko@hotmail.co.uk>
2015-04-08 22:00:11 +0100

    adding .classpath to gitignore file + have also added some more bits within interface package

--- src/interfaces/App.java
+++ src/interfaces/App.java
@@ -10,6 +10,41 @@ public class App {
         Person person1 = new Person("Richard");
         person1.greet();
 
+        //so the below bit is what i don't get...
+
+        Info info1 = new Machine();
+
+        //I can do the above because Machine implements the Info interface
+        //but my main question is why would you want (where is it useful) to define the variable as type Interface? I don't quite understand...why isn't info1 variable of type Machine?
+        //How is the memory management working here also? One to talk through with Jay.
+
+        info1.showInfo();
+
+        //because the info1 variable is of type Info, I can only access the overriding methods within Machine that are implemented from the Info interface.
+        //So i cannot see the non-overriding methods of Machine when I do info1.
+        //So for example, I couldn't do info1.start - wouldn't be valid
+
+        Info info2 = person1;
+
+        //I can do this because person1 is a reference to a person object (which implements the Info interface)
+        //So here, info2 would be pointing at the same thing/object as person1
+
+        info2.showInfo();
+
+        //So the above is going to run the overriding showInfo method within the Person class.
+
+        //Need to come back to the below. It is intrinsically linked to the method below outputInfo
+        //Not sure I understand this. Comes from the Udemy video at around 12 mins
+
+        System.out.println();
+
+        outputInfo(mach1);
+        outputInfo(person1);
+        //So in the above, I am passing mach1 and person1 respectively into the outputInfo method as arguments
+    }
+
+    private static void outputInfo(Info info){
+        info.showInfo();
     }
 
 }
--- src/interfaces/Person.java
+++ src/interfaces/Person.java
@@ -1,7 +1,8 @@
 package interfaces;
 
-public class Person implements Info {
+public class Person implements Info, SecondInterface {
     //So the implements basically means - take the methods in the interface "Info" and make sure they are used here.
+    //A class can implement multiple interfaces at once. But can only extend from one parent class.
 
     private String name;
 
@@ -23,4 +24,9 @@ public class Person implements Info {
     //If you don't have the @Override and then spelt the method incorrectly e.g. showsInfo, then this would not be an
     //overriding method. It would instead be just another new method.
     //So always leave in
+
+    @Override
+    public void interfaceTestMethod() {
+        System.out.println("This is a overridden method from the interface - SecondInterface");
+    }
 }

c8dbfc0347366c53ba0372d4c4c4a45a6aeae1c0
rchernanko <richard.chernanko@marks-and-spencer.com>
2015-04-09 21:54:47 +0100

    updating .gitignore file and also added some more code in preparation for toString chat on Monday

--- src/ToStringMethodExample_1.java
+++ src/ToStringMethodExample_1.java
@@ -10,13 +10,22 @@ public class ToStringMethodExample_1 {
             this.city=city;
         }
 
-    public String toString(){//overriding the toString() method
-        return rollno+" "+name+" "+city;
+//    public String toString(){//overriding the toString() method
+//        return rollno+" "+name+" "+city;
+//    }
+
+    //With the above, because there is a + (and they are string values), every time you use the +, you are creating a new version of the object
+    //see stringbuilder practice for more information. concatenating strings. every time you have a + between 2 strings, you're creating 2 string objects.
+    //bad from a memory management perspective
+    //there is instead another way to do the above, which is more efficient:
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(rollno).append(" ").append(name).append(" ").append(city);
+        return sb.toString();
     }
-
-    //another way to do the above, which is more efficient, is as below:
-
-
 
     public static void main(String args[]){
             ToStringMethodExample_1 s1=new ToStringMethodExample_1(101,"Raj","lucknow");
@@ -26,5 +35,3 @@ public class ToStringMethodExample_1 {
             System.out.println(s2);//if i don't have the overriding toString() method, here the compiler writes here s2.toString()
         }
     }
-
-//The above was taken from StackOverflow

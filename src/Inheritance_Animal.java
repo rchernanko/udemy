11cc76c44321b8c508d51610e41a16b10ddd4d79
rchernanko <richard.chernanko@marks-and-spencer.com>
2015-04-14 21:22:01 +0100

    more inheritance practice, 2 new classes

---
+++ src/Inheritance_Animal.java
@@ -0,0 +1,38 @@
+public class Inheritance_Animal {
+
+    private String animalName;
+    private boolean animalHasATail;
+    private int animalAge;
+
+    public void setAnimalName(String animalName){
+        this.animalName = animalName;
+    }
+
+    public String getAnimalName(){
+        return this.animalName;
+    }
+
+    public void setAnimalHasATail(boolean animalHasATail){
+        this.animalHasATail = animalHasATail;
+    }
+
+    public boolean getAnimalHasATail(){
+        return this.animalHasATail;
+    }
+
+    public void setAnimalAge(int animalAge){
+        this.animalAge = animalAge;
+    }
+
+    public int getAnimalAge(){
+        return this.animalAge;
+    }
+
+    public void animalRoars(){
+        System.out.println("ROOOOOOOAAAARRRR");
+    }
+
+    public void animalGoesToSleep(){
+        System.out.println("Tonight I'm going to sleep at 9pm");
+    }
+}

b67d7325d4d293767eae0d2517d6c33f5c6ba8cc
rchernanko <richard.chernanko@marks-and-spencer.com>
2015-04-21 08:36:04 +0100

    further work on the private, public and protected udemy tutorials

---
+++ src/publicprotectedprivate/world/Oak.java
@@ -0,0 +1,18 @@
+package publicprotectedprivate.world;
+
+public class Oak extends Plant {
+
+    public Oak(){
+
+        //The next line won't work
+        //type = "I am a type"
+        //For reference, 'type' is a private variable in the Plant class.
+        //Even though 'Oak' extends the 'Plant' class, it can still not access the 'type' variable.
+
+        this.size = "Medium";
+
+        //So what's going on above? Well, size is a 'protected' variable from the Plant class.
+        //And because Oak extends the Plant class (is a subclass of Plant), I can access the protected variables from Plant in here.
+
+    }
+}

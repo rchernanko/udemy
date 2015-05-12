9c0ab966349e0d7dc55b2a551dfa0ac98d0c37f1
Richard Chernanko <rchernanko@hotmail.co.uk>
2015-05-12 07:55:14 +0100

    exception handling classes - not finished though

---
+++ src/exception_handling/Demo3.java
@@ -0,0 +1,16 @@
+package exception_handling;
+
+import java.io.File;
+import java.io.FileReader;
+
+public class Demo3 {
+
+    public static void main(String[] args) {
+
+        File file = new File("test.txt");
+        FileReader fr = new FileReader(file);
+    }
+}
+
+
+//Up to 4.36 left

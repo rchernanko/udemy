268a55e404a185f8192b77b8d18c94f04f415efc
rchernanko <rchernanko@hotmail.co.uk>
2015-05-29 09:34:03 +0100

    finishing multiple exception handling tutorial

--- src/exception_handling_multiple_exceptions/Test.java
+++ src/exception_handling_multiple_exceptions/Test.java
@@ -1,11 +1,12 @@
 /*
 
-This tutorial looks at how to throw multiple exceptions from a method and also how to handle multiple them
+This tutorial looks at how to throw multiple exceptions from a method and also how to handle them
 
  */
 
 package exception_handling_multiple_exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
 
@@ -18,18 +19,28 @@ public class Test {
     }
 
     //But what if I want to throw another exception in the above method?
+
     //In the below, let's say that the IOException wasn't thrown but that something else was...
+    //FYI - I have commented out the "throw new IOException();" below but let's imagine that it's not
 
     public void run2() throws IOException, ParseException {
         // throw new IOException();
         throw new ParseException("Error message being thrown", 2);
     }
 
-    //In the above, you can see that there is a comma separated list of exceptions next to the method name
-    //What this means is that you COULD throw either an IOException or a ParseException if either one is detected
+    //In the above, you can see that there is a comma separated list of exceptions in the method signature
+    //What this means is that the run2 method COULD throw either an IOException or a ParseException if either one is detected
     //Note that you will only ever throw one exception at a time, you can't throw more than one
 
-    //So we have now "thrown" the exceptions back to the main method (in App.java in this package), let's see how
+    //So run2 has now "thrown" the exceptions back to the main method (in App.java in this package), let's see how
     //we should handle this (go to App.java)
+
+    //The below method is used to demonstrate a little complication when handling multiple exceptions (involves Polymorphism)
+    //The below needs to be read in conjunction with the App.java class in this package
+
+    public void input() throws IOException, FileNotFoundException{
+    }
+
+    //In the above method, FileNotFoundException is a child class of IOException
 
 }

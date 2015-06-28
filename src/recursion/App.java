4f598f6247bac60d3c0df76352205cae52e8e9e0
rchernanko <rchernanko@hotmail.co.uk>
2015-06-28 19:04:13 +0100

    final part of recursion video tutorial. don't understand the culculateFactorialNumber method so need to ask one of the others

--- src/recursion/App.java
+++ src/recursion/App.java
@@ -5,7 +5,10 @@ public class App {
     public static void main(String[] args) {
         int number = 4;
         calculate(number);
-        calculateAgain(number);
+//        calculateAgain(number);
+//        calculateAgain2(number);
+        System.out.println(calculateFactorialNumber(number));
+
     }
 
     //The reason for the below method, calculate, being static is that I do not want to create an object instance
@@ -26,27 +29,63 @@ public class App {
 
     ////// RECURSION //////
 
-    //So the e.g. calculate method can call itself and this is what is known as recursion.
+    //So the calculate method can call itself and this is what is known as recursion.
     //You call a method 'recursively'
 
     //Have a look in my calculateAgain method below - I am calling the calculateAgain method recursively
 
-    private static void calculateAgain(int initialNumber) {
-        System.out.println(initialNumber);
-        calculateAgain(initialNumber);
-    }
+//    private static void calculateAgain(int initialNumber) {
+//        System.out.println(initialNumber);
+//        calculateAgain(initialNumber - 1);
+//    }
 
     //But if we run the above, we will get an error, a StackOverflowError
     //"Exception in thread "main" java.lang.StackOverflowError"
 
-    //When you call a method from within another method, there is a special area of memory in the JVM called the "Stack"
+    //When you call a method from within another method, there is a special area of memory in the JVM called the
+    //"Stack", which is used.
     //The Stack is used for local variables and remembering which methods called which methods (so that they return
     //successfully etc)
 
-    //The Stack also have many other uses that no doubt I will discover while reading head first java.
+    //The Stack also has many other uses that no doubt I will discover while reading head first java.
     //The Stack is different to the "Heap" in that the Heap stores objects (i.e. when you write "new App();")
 
     //And so in the above, we are seeing the StackOverflowError because we are calling the calculateAgain method
     //recursively an infinite amount of times - we are not telling it to stop. And the Stack doesn't have enough memory
-    //to keep supporting this request!
+    //to keep supporting this request (I think this is the correct reasoning anyway!).
+
+    //Of course, getting "calculateAgain" to recursively call itself is fine, providing that there is some sort of way
+    //for it (calculateAgain) to know that it can stop calling itself. So what we could do is add an IF statement:
+
+    //(Again, I am creating another calculateAgain method below because I don't want to lose the stuff above!):
+
+    private static void calculateAgain2(int initialNumber) {
+        System.out.println(initialNumber);
+        if(initialNumber == 1) {
+            return;
+            //Even though the method uses "void", if I write "return" on it's own, nothing will actually be returned
+            //, and the code will still compile
+        }
+        calculateAgain2(initialNumber - 1);
+    }
+
+    //We can do useful stuff with recursion, a classic example being how to calculate a factorial value
+
+    //A factorial value is:
+    //e.g. Factorial 4 = 4x3x2x1
+    //e.g. Factorial 5 = 5x4x3x2x1
+    //etc etc
+
+    private static int calculateFactorialNumber(int factorial) {
+        System.out.println(factorial);
+        if(factorial == 1) {
+            return 1;
+        }
+        return calculateFactorialNumber(factorial - 1) * factorial;
+    }
+
+    //John also recommends looking at the Tower of Hanoi puzzle and trying to replicate that!
+
+    //I cannot get my head around how calculateFactorialNumber prints out 24....perhaps need to add to 'to do list' and
+    //ask one of the others at work...
 }
